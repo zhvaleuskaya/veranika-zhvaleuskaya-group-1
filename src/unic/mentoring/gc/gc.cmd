ac80985f747d9d32b3a164da3f016309af4e0774
Oleg Lazovski <olazovski@ecommerceaccel.com>
2014-10-14 00:04:07 +0300

    Test thread; cmd enhancement.

--- src/unic/mentoring/gc/gc.cmd
+++ src/unic/mentoring/gc/gc.cmd
@@ -1,1 +1,1 @@
-java unic.mentoring.gc.Run -Xms4m -Xmx16m -Xmn2m -XX:PermSize=12m -XX:MaxPermSize=18m -XX:+UseG1GC
+java unic.mentoring.gc.Run -Xms4m -Xmx16m -Xmn2m -XX:PermSize=12m -XX:MaxPermSize=18m -XX:+UseG1GC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps
