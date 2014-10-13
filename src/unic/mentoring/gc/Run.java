b0d0b0ad54135d7816bafc90102563bc39f4bd40
Oleg Lazovski <olazovski@ecommerceaccel.com>
2014-10-13 12:59:43 +0300

    Cmd for starting GC with custom parameters.

---
+++ src/unic/mentoring/gc/Run.java
@@ -0,0 +1,12 @@
+package unic.mentoring.gc;
+
+/**
+ * @author aleh_lazouski
+ */
+public class Run
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, garbage collector!");
+	}
+}
---
+++ src/unic/mentoring/gc/gc.cmd
@@ -0,0 +1,1 @@
+java unic.mentoring.gc.Run -Xms4m -Xmx16m -Xmn2m -XX:PermSize=12m -XX:MaxPermSize=18m -XX:+UseG1GC
